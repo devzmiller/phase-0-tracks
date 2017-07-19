41bdeeac02255302e85a1286902dc97f0b2a2952
Devin Miller <devzmiller@gmail.com>
2017-07-18 17:27:27 -0700

    Converts string inputs to other data types.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -8,13 +8,27 @@ puts "What is the hamster's name?"
 hamster_name = gets.chomp
 
 puts "What is the hamster's volume level (1-10)?"
-volume = gets.chomp
+volume = gets.chomp.to_i
 
 puts "What is the hamster's fur color?"
 color = gets.chomp
 
 puts "Is the hamster a good candidate for adoption?"
 adoption = gets.chomp
+if adoption[0] == "y" 
+  adoption = true
+else
+  adoption = false
+end
 
 puts "What is the hamster's estimated age?"
-age = gets.chomp
+age = gets.chomp.to_i
+if age == 0
+  age = nil
+end
+
+p hamster_name
+p volume
+p color
+p adoption
+p age
