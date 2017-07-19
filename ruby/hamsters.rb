c74e95e3d8f4bb2dbb5e365119826300b8487b1b
Devin Miller <devzmiller@gmail.com>
2017-07-18 17:19:22 -0700

    Adds hamster input to hamsters.rb (release 1)

---
+++ ruby/hamsters.rb
@@ -0,0 +1,20 @@
+# hamster's name
+# volume level 1 to 10
+# fur color
+# whether hamster is good candidate for adoption
+# estimated age
+
+puts "What is the hamster's name?"
+hamster_name = gets.chomp
+
+puts "What is the hamster's volume level (1-10)?"
+volume = gets.chomp
+
+puts "What is the hamster's fur color?"
+color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption?"
+adoption = gets.chomp
+
+puts "What is the hamster's estimated age?"
+age = gets.chomp
