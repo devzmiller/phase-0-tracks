5fe809d1dbc1ead507f941e5f7daff9b78b47af5
Devin Miller <devzmiller@gmail.com>
2017-08-04 19:36:26 -0700

    Creates mixin Shout module and Flight module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,40 @@
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "!!!" + " :D"
+#   end
+# end
+
+# puts Shout.yell_angrily("Fishsticks")
+# puts Shout.yell_happily("Walruses")
+
 module Shout
 
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!!" + " :D"
   end
 end
 
-puts Shout.yell_angrily("Fishsticks")
-puts Shout.yell_happily("Walruses")
+class Howler_Monkey
+  include Shout
+end
+
+class Manager
+  include Shout
+end
+
+monkey = Howler_Monkey.new
+puts monkey.yell_happily("Bananas")
+puts monkey.yell_angrily("Lions")
+
+boss = Manager.new
+puts boss.yell_angrily("Why didn't you meet your deadline?")
+puts boss.yell_happily("You sold all the things")
